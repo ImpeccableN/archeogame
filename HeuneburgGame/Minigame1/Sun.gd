25818da5ebad8beb99bd3499d96af83608984477
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-05-17 17:49:59 +0200

    added Sun movement

--- HeuneburgGame/Minigame1/Sun.gd
+++ HeuneburgGame/Minigame1/Sun.gd
@@ -1,4 +1,26 @@
 extends Area2D
 
 export var speed = 400.0
+var straight = true
+var screen_size = Vector2.ZERO
 
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _process(delta):
+	var direction = Vector2.ZERO
+	
+	if straight:
+		direction.x += 1
+	else:
+		direction.x -= 1
+	
+	position.x += direction.x * speed * delta
+	
+	if position.x > screen_size.x:
+		straight = not straight
+		position.y += 64
+	
+	if position.x < 0:
+		straight = not straight
+		position.y += 64
