603f4fa5267d290b1055635fd2159a3c5ba97a6d
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-05-17 15:22:17 +0200

    finished scripting Player movement

--- HeuneburgGame/Minigame1/Player.gd
+++ HeuneburgGame/Minigame1/Player.gd
@@ -2,15 +2,23 @@ extends Area2D
 
 
 export var speed = 400.0
-export var lineHeight = 50
+export var lineHeight = 64.0
 
 
 func _process(delta):
 	var direction = Vector2.ZERO
 	if Input.is_action_pressed("move_right"):
-		direction.x += 1 * speed
+		direction.x += 1
 	if Input.is_action_pressed("move_left"):
-		direction.x -= 1 * speed
+		direction.x -= 1
 	
-	if Input.is_action_pressed("move_down"):
-		direction.y += lineHeight
+	if Input.is_action_just_pressed("move_down"):
+		direction.y += 1
+	if Input.is_action_just_pressed("move_up"):
+		direction.y -= 1
+	
+	if direction.length() > 1:
+		direction = direction.normalized();
+	
+	position.x += direction.x * delta * speed
+	position.y += direction.y * lineHeight
