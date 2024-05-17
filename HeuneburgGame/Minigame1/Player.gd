5edab34e3c71ba377e10946fc3c49be4bb97ce7b
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-05-17 15:35:05 +0200

    clamped Player position between screen borders

--- HeuneburgGame/Minigame1/Player.gd
+++ HeuneburgGame/Minigame1/Player.gd
@@ -2,7 +2,11 @@ extends Area2D
 
 
 export var speed = 400.0
-export var lineHeight = 64.0
+export var line_height = 64.0
+var screen_size = Vector2.ZERO
+
+func _ready():
+	screen_size = get_viewport_rect().size
 
 
 func _process(delta):
@@ -21,4 +25,7 @@ func _process(delta):
 		direction = direction.normalized();
 	
 	position.x += direction.x * delta * speed
-	position.y += direction.y * lineHeight
+	position.y += direction.y * line_height
+	position.x = clamp(position.x, 32, screen_size.x - 32)
+	position.y = clamp(position.y, 32, screen_size.y - 32)
+	
