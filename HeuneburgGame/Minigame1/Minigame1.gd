cad5355099ebd8443fc8fcc90304b556ca36eeea
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-05-19 10:21:50 +0200

    Minigame1: changed Sun to Truck

--- HeuneburgGame/Minigame1/Minigame1.gd
+++ HeuneburgGame/Minigame1/Minigame1.gd
@@ -1,11 +1,11 @@
 extends Node
 
-var sun_position = Vector2.ZERO
-onready var sun_node = get_node("Sun")
+var truck_position = Vector2.ZERO
+onready var truck_node = get_node("Truck")
 onready var tilemap = get_node("TileMap")
 
 func _process(delta):
-	sun_position = sun_node.position
-	var sunny_tile : Vector2 = tilemap.world_to_map(sun_position)
-	tilemap.set_cellv(sunny_tile, 0)
+	truck_position = truck_node.position
+	var trucked_tile : Vector2 = tilemap.world_to_map(truck_position)
+	tilemap.set_cellv(trucked_tile, 1)
 	
