7f369fcd04e027ee5db2112e0afbbccf4cb8fe79
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-05-19 11:04:23 +0200

    implemented movement to Sun

---
+++ HeuneburgGame/Minigame1/Sun.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+export var speed = 500.0
+onready var screen_size = get_viewport_rect().size
+
+var direction := Vector2(1.0, 0.75)
+
+
+func _process(delta):
+	position += direction * delta * speed
+	if position.y > screen_size.y:
+		direction.y = -0.75
+	if position.y < 0:
+		direction.y = 0.75
+	if position.x > screen_size.x:
+		direction.x = -1.0
+	if position.x < 0:
+		direction.x = 1.0
