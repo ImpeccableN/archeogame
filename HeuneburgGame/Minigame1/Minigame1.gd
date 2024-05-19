d78fff637562f4ea548de98429cea4cf42b775ec
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-05-19 08:46:57 +0200

    added tilechange through suntouch in Minigame1 script

--- HeuneburgGame/Minigame1/Minigame1.gd
+++ HeuneburgGame/Minigame1/Minigame1.gd
@@ -1,4 +1,11 @@
 extends Node
 
 var sun_position = Vector2.ZERO
+onready var sun_node = get_node("Sun")
+onready var tilemap = get_node("TileMap")
 
+func _process(delta):
+	sun_position = sun_node.position
+	var sunny_tile : Vector2 = tilemap.world_to_map(sun_position)
+	tilemap.set_cellv(sunny_tile, 0)
+	
