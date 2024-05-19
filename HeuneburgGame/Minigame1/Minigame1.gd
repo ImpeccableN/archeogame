4b0aecd65a1e7670db17106c43d504adad06d026
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-05-19 12:55:20 +0200

    Minigame1: truck spawns artefacts, prototype

--- HeuneburgGame/Minigame1/Minigame1.gd
+++ HeuneburgGame/Minigame1/Minigame1.gd
@@ -5,6 +5,7 @@ var sun_position = Vector2.ZERO
 onready var truck_node = get_node("Truck")
 onready var tilemap = get_node("TileMap")
 onready var sun_node = get_node("Sun")
+export (PackedScene) var artefact_scene
 
 func _process(delta):
 	truck_position = truck_node.position
@@ -14,3 +15,9 @@ func _process(delta):
 	sun_position = sun_node.position
 	var sunny_tile : Vector2 = tilemap.world_to_map(sun_position)
 	tilemap.set_cellv(sunny_tile, 0)
+
+
+func _on_ArtefactSpawnTimer_timeout():
+	truck_position = truck_node.position
+	var artefact_tile : Vector2 = tilemap.world_to_map(Vector2(truck_position.x -50, truck_position.y))
+	tilemap.set_cellv(artefact_tile, 2)
