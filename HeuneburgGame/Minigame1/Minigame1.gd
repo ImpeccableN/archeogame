b96d1b41f2b2a8fbb1e56d4f19a72d28390c1541
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-05-19 11:09:20 +0200

    tiles change to dry with sun touch

--- HeuneburgGame/Minigame1/Minigame1.gd
+++ HeuneburgGame/Minigame1/Minigame1.gd
@@ -1,11 +1,16 @@
 extends Node
 
 var truck_position = Vector2.ZERO
+var sun_position = Vector2.ZERO
 onready var truck_node = get_node("Truck")
 onready var tilemap = get_node("TileMap")
+onready var sun_node = get_node("Sun")
 
 func _process(delta):
 	truck_position = truck_node.position
 	var trucked_tile : Vector2 = tilemap.world_to_map(truck_position)
 	tilemap.set_cellv(trucked_tile, 1)
 	
+	sun_position = sun_node.position
+	var sunny_tile : Vector2 = tilemap.world_to_map(sun_position)
+	tilemap.set_cellv(sunny_tile, 0)
