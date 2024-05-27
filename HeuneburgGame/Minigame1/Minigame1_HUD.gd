edecf091569fee0e4dc86ce72ad6380eca6a309d
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-05-27 13:45:24 +0200

    Minigame1: StartButton disabled on game start and abled on player hit

--- HeuneburgGame/Minigame1/Minigame1_HUD.gd
+++ HeuneburgGame/Minigame1/Minigame1_HUD.gd
@@ -7,3 +7,13 @@ onready var score_label = $ScoreLabel
 func _on_Minigame1_score_up(score):
 	score_label.text = str(score)
 	
+
+
+func _on_Player_hit():
+	$StartButton.show()
+	$StartButton.disabled = false
+
+
+func _on_StartButton_button_up():
+	$StartButton.hide()
+	$StartButton.disabled = true
