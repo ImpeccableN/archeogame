9ee19b2a3b3b3cd4f37e01978c6968f1d7b86807
ch1190 <hennicke@protonmail.com>
2024-05-27 22:05:18 +0200

    Main + Pause Menu
    First simple Main Menu and Pause Menu (ESC), expandable

---
+++ HeuneburgGameUI/MainMenu.gd
@@ -0,0 +1,13 @@
+extends Control
+
+
+func _on_Start_pressed():
+	get_tree().change_scene("res://Level/Level0.tscn")
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
+
+func _on_Credits_pressed():
+	pass # Replace with function body.
