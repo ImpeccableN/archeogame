060152a937169db1c59cdc3f5cbde1262e8ff8f6
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-05-28 15:56:38 +0200

    Minigame1: added script to delete all marker on new game start

--- HeuneburgGame/Minigame1/Minigame1.gd
+++ HeuneburgGame/Minigame1/Minigame1.gd
@@ -5,7 +5,8 @@ var sun_position = Vector2.ZERO
 var sun_direction = true
 var marker_scene = preload("res://Minigame1/Marker.tscn")
 var score = 0
-var cell_array
+var cell_array : Array
+var artefact_array : Array
 onready var truck_node = get_node("Truck")
 onready var tilemap = get_node("TileMap")
 onready var sun_node = get_node("Sun")
@@ -48,6 +49,7 @@ func _on_Player_set_marker(player_position):
 	if tilemap.get_cellv(player_tile) == 2:
 		var marker_instace = marker_scene.instance()
 		add_child(marker_instace)
+		artefact_array.append(marker_instace)
 		marker_instace.position = tilemap.map_to_world(player_tile) + Vector2(32, 32)
 		score += 1
 		emit_signal("score_up", score)
@@ -61,8 +63,12 @@ func _on_Player_hit():
 
 
 func _on_Minigame1_HUD_start_game():
+	# set all tilemap cells back to dryland tiles
 	for cell in cell_array:
 		tilemap.set_cellv(cell, 0)
+	#delete all markers on map
+	for artefact in artefact_array:
+		artefact.queue_free()
 	$Truck/CollisionShape2D.disabled = false
 	$Sun/CollisionShape2D.disabled = false
-	tilemap
+	
