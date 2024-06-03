5188fd119e51e542c2884c9bd75715d71dff62ee
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-06-03 15:09:43 +0200

    FirstLevel_Map: Minigame1 opens with click on grabungsstaette

--- HeuneburgGame/Level1/Button_Grabungsstaette.gd
+++ HeuneburgGame/Level1/Button_Grabungsstaette.gd
@@ -4,4 +4,4 @@ extends Area2D
 
 func _on_Button_Grabungsstaette_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.is_pressed():
-		print("clicekdiclick")
+		get_tree().change_scene("res://Minigame1/Main_Minigame1.tscn")
