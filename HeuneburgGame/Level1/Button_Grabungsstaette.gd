231a826f66d386a09cc208d4ff1b0aaccd8b3432
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-06-06 12:15:34 +0200

    Added some commentary

--- HeuneburgGame/Level1/Button_Grabungsstaette.gd
+++ HeuneburgGame/Level1/Button_Grabungsstaette.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 
-
+#checks if Input event on area is mousebuttonclick. if so change scene to minigame1
 func _on_Button_Grabungsstaette_input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.is_pressed():
 		get_tree().change_scene("res://Minigame1/Main_Minigame1.tscn")
--- HeuneburgGame/Minigame1/Artefact.gd
+++ HeuneburgGame/Minigame1/Artefact.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 
 
-
+#checks if bodys entering Artefact area belong to group "sun". If so, delete Artefact scene
 func _on_Artefact_body_entered(body):
 	var groups_of_colliding_body = body.get_groups()
 	for group in groups_of_colliding_body:
