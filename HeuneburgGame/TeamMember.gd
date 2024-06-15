9dbc30acfbe1730fcf5a0fe8468b02d941b93706
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-06-15 18:05:17 +0200

    TeamMember scene: started to work on _ready function

--- HeuneburgGame/TeamMember.gd
+++ HeuneburgGame/TeamMember.gd
@@ -1,2 +1,10 @@
 extends Node
 
+export var member_name = ""
+var member_image_path = ""
+
+#more properties to come
+
+func _ready():
+	#create image path with path to folder plus member_name var
+	member_image_path = 
