ff12cbd44732258c1c79692c2f4fe4173d9a4bd7
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-06-16 13:46:52 +0200

    MinigameWerkzeuge: added script to load all members in

--- HeuneburgGame/Minigame_WerkzeugZuordnen/Main_Minigame_WerkzeugZuordnen.gd
+++ HeuneburgGame/Minigame_WerkzeugZuordnen/Main_Minigame_WerkzeugZuordnen.gd
@@ -2,12 +2,15 @@ extends Node2D
 
 var team_members_array : Array
 var which_team = 1
+var team_members_path = ""
 
 func _ready():
 	if which_team == 1:
-		get_team_members("res://Minigame_WerkzeugZuordnen/WerkzeugTeamMembers/Team1")
+		team_members_path = "res://Minigame_WerkzeugZuordnen/WerkzeugTeamMembers/Team1/"
 	elif which_team == 2:
-		get_team_members("res://Minigame_WerkzeugZuordnen/WerkzeugTeamMembers/Team2")
+		team_members_path = "res://Minigame_WerkzeugZuordnen/WerkzeugTeamMembers/Team2/"
+	get_team_members(team_members_path)
+	load_in_team_members()
 
 func get_team_members(path):
 	var dir = Directory.new()
@@ -24,3 +27,14 @@ func get_team_members(path):
 	else:
 		print("An error occurred when trying to access the path.")
 	print(team_members_array)
+
+func load_in_team_members():
+	var positions = $Teampositions.get_children()
+	var i = 0
+	for member in team_members_array:
+		var member_scene = load(team_members_path + member)
+		var member_scene_instance = member_scene.instance()
+		add_child(member_scene_instance)
+		member_scene_instance.position = positions[i].position
+		i += 1
+	
