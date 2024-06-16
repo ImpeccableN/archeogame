0ac505dabadc0c8152e79c60b0df3483351db2f9
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-06-16 13:07:57 +0200

    MinigameWerkzeug: added get_team_members function to MainMinigame script. loads team member scenes from folder

--- HeuneburgGame/Minigame_WerkzeugZuordnen/Main_Minigame_WerkzeugZuordnen.gd
+++ HeuneburgGame/Minigame_WerkzeugZuordnen/Main_Minigame_WerkzeugZuordnen.gd
@@ -1,2 +1,26 @@
 extends Node2D
 
+var team_members_array : Array
+var which_team = 1
+
+func _ready():
+	if which_team == 1:
+		get_team_members("res://Minigame_WerkzeugZuordnen/WerkzeugTeamMembers/Team1")
+	elif which_team == 2:
+		get_team_members("res://Minigame_WerkzeugZuordnen/WerkzeugTeamMembers/Team2")
+
+func get_team_members(path):
+	var dir = Directory.new()
+	if dir.open(path) == OK:
+		dir.list_dir_begin(true)
+		var file_name = dir.get_next()
+		
+		while file_name != "":
+			if dir.current_is_dir():
+				pass
+			elif file_name.get_extension() == "tscn":
+				team_members_array.append(file_name)
+			file_name = dir.get_next()
+	else:
+		print("An error occurred when trying to access the path.")
+	print(team_members_array)
