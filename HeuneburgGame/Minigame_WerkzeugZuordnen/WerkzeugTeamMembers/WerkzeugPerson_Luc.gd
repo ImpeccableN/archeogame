5602f4fe2ed893ca7a937637ea92101f59c2973c
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-06-16 11:31:55 +0200

    Minigame_Werkzeuge: created alll teamMember scenes for this game

---
+++ HeuneburgGame/Minigame_WerkzeugZuordnen/WerkzeugTeamMembers/WerkzeugPerson_Luc.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+#first add TeamMember as Node to tree. Then team_mamber_data var equals TeamMember Node
+onready var team_member_data = $TeamMember_Luc
+
+func _ready():
+	print(team_member_data.member_headshot_path)
+	$Sprite.texture = load(team_member_data.member_headshot_path)
+
+func _on_WerkzeugPerson_mouse_entered():
+	$Sprite/InfoBox.show()
+
+
+func _on_WerkzeugPerson_mouse_exited():
+	$Sprite/InfoBox.hide()
