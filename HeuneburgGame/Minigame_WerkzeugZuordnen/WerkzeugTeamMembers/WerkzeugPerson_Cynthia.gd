781138d0ed2d172c17e86bb34dc9c0b5c85e0e3f
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-06-16 10:59:43 +0200

    created all teammembers as TeamMember Scenes

--- HeuneburgGame/Minigame_WerkzeugZuordnen/WerkzeugTeamMembers/WerkzeugPerson_Cynthia.gd
+++ HeuneburgGame/Minigame_WerkzeugZuordnen/WerkzeugTeamMembers/WerkzeugPerson_Cynthia.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
 #first add TeamMember as Node to tree. Then team_mamber_data var equals TeamMember Node
-#onready var team_member_data = 
+onready var team_member_data = $TeamMember_Cynthia
 
-#func _ready():
-#	print(team_member_data.member_headshot_path)
-#	$Sprite.texture = load(team_member_data.member_headshot_path)
+func _ready():
+	print(team_member_data.member_headshot_path)
+	$Sprite.texture = load(team_member_data.member_headshot_path)
 
 func _on_WerkzeugPerson_mouse_entered():
 	$Sprite/InfoBox.show()
--- HeuneburgGame/TeamMember.gd
+++ HeuneburgGame/TeamMember.gd
@@ -9,6 +9,8 @@ var member_fullcard_bw_path = ""
 #more properties to come
 
 func _ready():
-	member_headshot_path = image_folder_path + "/Headshots/" + member_name + ".png"
-	member_fullcard_path = image_folder_path + "/FullCards/" + member_name + ".png"
-	member_fullcard_bw_path = image_folder_path + "/FullCards/" + member_name + "_bw.png"
+	member_headshot_path = image_folder_path + "Headshots/" + member_name + ".png"
+	member_fullcard_path = image_folder_path + "FullCards/" + member_name + ".png"
+	member_fullcard_bw_path = image_folder_path + "FullCards/" + member_name + "_bw.png"
+
+
