0b2676a42bbb76ed52f4c5a8fb8212895666deaa
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-06-21 12:59:07 +0200

    Level1Map: added bauwagen image plus growing when hovering over it

--- HeuneburgGame/Level1/FirstLevel_Map.gd
+++ HeuneburgGame/Level1/FirstLevel_Map.gd
@@ -3,6 +3,7 @@ extends Node2D
 onready var grabungsstaette_image = get_node("Grabungsstaette_Overlay")
 onready var freilichtmuseum_image = get_node("Freilichtmuseum_Overlay")
 onready var keltenmuseum_image = get_node("Keltenmuseum_Overlay")
+onready var bauwagen_image = get_node("Bauwagen_Overlay")
 export var images_grow_scale := Vector2.ZERO
 
 
@@ -28,3 +29,11 @@ func _on_Button_Keltenmuseum_mouse_entered():
 
 func _on_Button_Keltenmuseum_mouse_exited():
 	keltenmuseum_image.rect_scale = Vector2.ONE
+
+
+func _on_Button_Bauwagen_mouse_entered():
+	bauwagen_image.rect_scale = images_grow_scale
+
+
+func _on_Button_Bauwagen_mouse_exited():
+	bauwagen_image.rect_scale = Vector2.ONE
