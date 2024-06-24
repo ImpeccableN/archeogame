39a27f987c961a5390e7b9784bbb5a6f00cca5bd
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-06-24 18:47:48 +0200

    MinigameWerkzeuge: ein Signal von der Werkzeugszene war mit irgendwas verbunden, das nicht existiert. Habe ich getrennt

--- HeuneburgGame/Level1/Button_Bauwagen.gd
+++ HeuneburgGame/Level1/Button_Bauwagen.gd
@@ -2,6 +2,6 @@ extends Area2D
 
 
 
-func _on_Button_Bauwagen_input_event(viewport, event, shape_idx):
+func _on_Button_Bauwagen_input_event(_viewport, event, _shape_idx):
 	if event is InputEventMouseButton and event.is_pressed():
 		get_tree().change_scene("res://Minigame_WerkzeugZuordnen/Main_Minigame_WerkzeugZuordnen.tscn")
--- HeuneburgGame/Minigame_WerkzeugZuordnen/Werkzeug.gd
+++ HeuneburgGame/Minigame_WerkzeugZuordnen/Werkzeug.gd
@@ -11,7 +11,7 @@ func _on_Werkzeug_mouse_exited():
 	mouse_entered = false
 
 
-func _process(delta):
+func _process(_delta):
 	if mouse_entered and Input.is_mouse_button_pressed(BUTTON_LEFT):
 		position = get_global_mouse_position()
 
