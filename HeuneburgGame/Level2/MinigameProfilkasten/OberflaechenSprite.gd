c2a0bd48352501e16a0ab5d2c4aff6bd1b1c02fe
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-08-02 16:24:27 +0200

    MinigameProfilkasten: habe ein paar Fortschritte gemacht. Befürchte, die Methode ist vielleicht nicht geeignet...

--- HeuneburgGame/Level2/MinigameProfilkasten/OberflaechenSprite.gd
+++ HeuneburgGame/Level2/MinigameProfilkasten/OberflaechenSprite.gd
@@ -11,29 +11,30 @@ func _ready():
 	for point in node.polygon:
 		point_arr.append(Transform2D(0, node.global_position) * point)
 	var global_poly = Polygon2D.new()
-	global_poly.polygon = point_arr
+	global_poly.polygon = PoolVector2Array(point_arr)
 	polygon_global = global_poly
 
 
 func _input(event):
 	if event.is_pressed() and event.get_button_index() == 1:
-		if Geometry.is_point_in_polygon(event.position, polygon_global.polygon):
+#		if Geometry.is_point_in_polygon(event.position, polygon_global.polygon):
+		if 1 == 1:
 			var offset_poly = Polygon2D.new()
-			var new_values : Array = []
+			var new_values = []
 			for point in clip_poly.polygon:
 				new_values.append(Transform2D(0, event.position) * point)
-			offset_poly.polygon = new_values
+			offset_poly.polygon = PoolVector2Array(new_values)
 			print("node polygon before: ")
 			print(node.polygon)
-			var splits = Geometry.clip_polygons_2d(node.polygon, offset_poly.polygon)
+			var splits = Geometry.clip_polygons_2d(polygon_global.polygon, offset_poly.polygon)
 			print("Splits: ")
 			print(splits)
-#		var lok_polygon : Array = []
-#		for point in splits[0]:
-#			lok_polygon.append(point - node.global_position)
-#			for poly in splits:
+			var lok_polygon = []
+			for point in splits[0]:
+				lok_polygon.append(point - node.global_position)
+#			for poly in splits[0]:
 #				if Geometry.is_polygon_clockwise(poly):
-			node.polygon = splits[0]
+			node.polygon = PoolVector2Array(lok_polygon)
+			polygon_global.polygon = PoolVector2Array(splits[0])
 			print("node polygon after: ")
 			print(node.polygon)
-			node.texture = textureload
