e650829bf2c834023c7b603c9be20a7857d7e093
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-08-05 16:43:21 +0200

    MinigameProfilkasten: Tried working with islands. texture wont show how i want it to

--- HeuneburgGame/Level2/MinigameProfilkasten/OberflaechenSprite.gd
+++ HeuneburgGame/Level2/MinigameProfilkasten/OberflaechenSprite.gd
@@ -19,28 +19,36 @@ func _input(event):
 	if event.is_pressed() and event.get_button_index() == 1:
 #		if Geometry.is_point_in_polygon(event.position, polygon_global.polygon):
 		if 1 == 1:
-			clip_poly.global_position = event.position
-			var offset_poly = Polygon2D.new()
-			var new_values = []
-			for point in clip_poly.polygon:
-				new_values.append(Transform2D(0, clip_poly.global_position) * point)
-			offset_poly.polygon = PoolVector2Array(new_values)
-			print("node polygon before: ")
-			print(node.polygon)
-			var splits = Geometry.clip_polygons_2d(polygon_global.polygon, offset_poly.polygon)
-			print("Splits: ")
-			print(splits)
-			var outer_polygon : int
-			if Geometry.is_polygon_clockwise(splits[0]):
-				outer_polygon = 1
-			else:
-				outer_polygon = 0
-			var lok_polygon = []
-			for point in splits[outer_polygon]:
-				lok_polygon.append(point - node.global_position)
-#			for poly in splits[0]:
-#				if Geometry.is_polygon_clockwise(poly):
-			node.polygon = PoolVector2Array(lok_polygon)
-			polygon_global.polygon = PoolVector2Array(splits[outer_polygon])
-			print("node polygon after: ")
-			print(node.polygon)
+			clip(event)
+
+
+func clip(event):
+	clip_poly.global_position = event.position
+	var offset_poly = Polygon2D.new()
+	var new_values = []
+	for point in clip_poly.polygon:
+		new_values.append(Transform2D(0, clip_poly.global_position) * point)
+	offset_poly.polygon = PoolVector2Array(new_values)
+#	print("node polygon before: ")
+#	print(node.polygon)
+	var splits = Geometry.clip_polygons_2d(polygon_global.polygon, offset_poly.polygon)
+#	print("Splits: ")
+#	print(splits)
+	var lok_polygon = []
+	for point in splits[0]:
+		lok_polygon.append(point - node.global_position)
+#	for poly in splits[0]:
+#		if Geometry.is_polygon_clockwise(poly):
+	node.polygon = PoolVector2Array(lok_polygon)
+	polygon_global.polygon = PoolVector2Array(splits[0])
+	if splits.size() > 1:
+		for point in splits[1]:
+			lok_polygon.clear()
+			lok_polygon.append(point - node.get_offset())
+		var new_poly = Polygon2D.new()
+		new_poly.polygon = PoolVector2Array(splits[1])
+		new_poly.set_texture(textureload)
+		new_poly.set_texture_offset(node.get_texture_offset())
+		new_poly.position = node.get_offset()
+		get_parent().add_child(new_poly)
+		new_poly.show()
