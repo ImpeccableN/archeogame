5e2562e8b7740ee6bcc568593aca1c0e3525b295
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-08-06 13:52:50 +0200

    MinigameProfilkasten: created ProfilkastenTool scene. You can grab it and the cursor disappears

---
+++ HeuneburgGame/Level2/MinigameProfilkasten/ProfilkastenTool.gd
@@ -0,0 +1,36 @@
+extends Area2D
+
+var grabbed : bool = false 
+var mouse_entered : bool = false
+
+func set_grabbed():
+	grabbed = not grabbed
+
+func get_grabbed():
+	return grabbed
+
+func show_cursor():
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_HIDDEN:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+func _input(event):
+	if mouse_entered:
+		if event.is_pressed() and event.get_button_index() == 1:
+			set_grabbed()
+			show_cursor()
+	
+	if grabbed and event is InputEventMouse:
+		
+		position = get_global_mouse_position()
+	
+	
+
+
+func _on_ProfilkastenTool_mouse_entered():
+	mouse_entered = true
+
+
+func _on_ProfilkastenTool_mouse_exited():
+	mouse_entered = false
