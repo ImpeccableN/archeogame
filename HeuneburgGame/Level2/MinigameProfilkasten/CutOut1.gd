ab646505d89d53b3b11c4ddf3d89133ad0a57357
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-08-10 13:36:56 +0200

    MinigameProfilkasten: MainNode checks if all cutouts are deleted

--- HeuneburgGame/Level2/MinigameProfilkasten/CutOut1.gd
+++ HeuneburgGame/Level2/MinigameProfilkasten/CutOut1.gd
@@ -2,6 +2,12 @@ extends TextureRect
 
 var shovel_entered : bool = false
 
+signal deleted(node)
+
+func _ready():
+	var main_node = get_parent().get_parent()
+	connect("deleted", main_node, "erase_cutout")
+
 func area_entered(area):
 	if "schaufel" in area.get_groups():
 		shovel_entered = true
@@ -17,4 +23,6 @@ func area_exited(area):
 
 func _process(delta):
 	if Input.is_mouse_button_pressed(1) and shovel_entered:
+		emit_signal("deleted", self)
 		queue_free()
+		
--- HeuneburgGame/Level2/MinigameProfilkasten/MinigameProfilkasten.gd
+++ HeuneburgGame/Level2/MinigameProfilkasten/MinigameProfilkasten.gd
@@ -1,11 +1,19 @@
 extends Node2D
 
+onready var cutout_arr: Array = get_tree().get_nodes_in_group("cutout")
+
 func _ready():
 	var tools: Array = get_tree().get_nodes_in_group("tool")
 	var tool_pos: Array = $Toolbox.get_children()
+	
 	
 	var i = 0
 	for tl in tools:
 		tl.position = tool_pos[i].global_position
 		tl.set_wait_pos(tool_pos[i].global_position)
 		i += 1
+
+func erase_cutout(node):
+	cutout_arr.erase(node)
+	if cutout_arr.size() == 0:
+		print("success")
