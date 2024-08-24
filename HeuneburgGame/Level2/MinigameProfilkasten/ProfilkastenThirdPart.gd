794ba7d08982f954be999bde81c8b671f4d9483b
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-08-24 16:05:25 +0200

    Profilkasten: you can take a picture now with the camera. gets saved on harddrive

--- HeuneburgGame/Level2/MinigameProfilkasten/ProfilkastenThirdPart.gd
+++ HeuneburgGame/Level2/MinigameProfilkasten/ProfilkastenThirdPart.gd
@@ -3,6 +3,7 @@ extends Node2D
 var grabbed_tool : Node = null
 export var strength: float
 export var speed: float
+export var region: Rect2
 onready var flash = get_node("CameraFlash")
 
 signal change_tool(node)
@@ -32,14 +33,20 @@ func _on_grabbed_tool(node):
 		emit_signal("change_tool", node)
 
 
+
 func camera_flash():
+	$Kamera.hide()
+	yield(get_tree().create_timer(0.1), "timeout")
+	var image = get_viewport().get_texture().get_data().get_rect(region)
+	image.flip_y()
+	image.save_png("D:/kraka/Studium/SoSe24/bilds/screeny6.png")
+	
+#	$Toolbox.show()
 	$AudioCameraFlash.play()
 	$Tween.interpolate_property(flash, "self_modulate:a", 0, strength, speed, Tween.TRANS_SINE, Tween.EASE_OUT)
 	$Tween.start()
-#	$AudioCameraFlash.play()
-#	$AudioCameraFlash.stream_paused = true
 	
 	yield(get_tree().create_timer(0.05), "timeout")
 	$Tween.interpolate_property(flash, "self_modulate:a", strength, 0, 1, Tween.TRANS_SINE, Tween.EASE_OUT)
 	$Tween.start()
-#	$AudioCameraFlash.stream_paused = true
+	$Kamera.show()
