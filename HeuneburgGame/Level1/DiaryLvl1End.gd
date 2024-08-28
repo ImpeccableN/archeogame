216cfad2f6c1f2debf75662003ecccbeb59d529d
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-08-28 20:57:07 +0200

    worked on uebergaenge beteween days

--- HeuneburgGame/Level1/DiaryLvl1End.gd
+++ HeuneburgGame/Level1/DiaryLvl1End.gd
@@ -4,3 +4,7 @@ extends TextureRect
 
 func _on_Button_nextDay_pressed():
 	get_tree().change_scene("res://Level2/DiaryLvl2.tscn")
+
+
+func _on_DiaryLvl1End_gui_input(event):
+	pass # Replace with function body.
--- HeuneburgGame/Level2/DiaryLvl2End.gd
+++ HeuneburgGame/Level2/DiaryLvl2End.gd
@@ -2,3 +2,7 @@ extends TextureRect
 
 func _ready():
 	$ProfilkastenPhoto.texture = Global.photo
+
+
+#func _on_Button_nextDay_pressed():
+#	get_tree().change_scene()
