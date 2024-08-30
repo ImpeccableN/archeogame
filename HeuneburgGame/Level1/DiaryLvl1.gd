29158210755d0678db9e6faeae0f2bb6184cae9e
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-08-30 16:40:19 +0200

    added close button in day 1 diary

--- HeuneburgGame/Level1/DiaryLvl1.gd
+++ HeuneburgGame/Level1/DiaryLvl1.gd
@@ -2,6 +2,6 @@ extends TextureRect
 
 
 
-func _on_DiaryLvl1_gui_input(event):
-	if event.is_action("click_button"):
-		get_tree().change_scene("res://Level1/FirstLevel_Map.tscn")
+
+func _on_CloseButton_pressed():
+	get_tree().change_scene("res://Level1/FirstLevel_Map.tscn")
