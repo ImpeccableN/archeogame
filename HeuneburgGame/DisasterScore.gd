3808ff0ffff4f62b977eef03d039fbb6e6d12836
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-09-07 15:37:20 +0200

    implemented disaster score in first map

--- HeuneburgGame/DisasterScore.gd
+++ HeuneburgGame/DisasterScore.gd
@@ -2,7 +2,7 @@ extends Sprite
 
 
 var disaster_score
-var load_num
+var load_num : String
 
 
 
@@ -11,28 +11,29 @@ func _ready():
 	
 	match_score()
 	load_texture()
-
+	print(disaster_score)
+	print(load_num)
 
 func match_score():
 	if disaster_score == 0:
-		load_num == 0
-	if disaster_score >= 0 && disaster_score <= 14:
-		load_num == 1
+		load_num = "0"
+	if disaster_score > 0 && disaster_score <= 14:
+		load_num = "1"
 	elif disaster_score > 14 && disaster_score <= 28:
-		load_num == 2
+		load_num = "2"
 	elif disaster_score > 28 && disaster_score <= 42:
-		load_num == 3
+		load_num = "3"
 	elif disaster_score > 42 && disaster_score <= 56:
-		load_num == 4
+		load_num = "4"
 	elif disaster_score > 56 && disaster_score <= 61:
-		load_num == 5
+		load_num = "5"
 	elif disaster_score > 62 && disaster_score <= 76:
-		load_num == 6
+		load_num = "6"
 	elif disaster_score > 76 && disaster_score <= 99:
-		load_num == 7
-	elif disaster_score <= 100:
-		load_num == 8
+		load_num = "7"
+	elif disaster_score >= 100:
+		load_num = "8"
 
 
 func load_texture():
-	texture = load("res://graphics/DisasterScoreBar/DisasterBar" +  str(load_num) + ".png")
+	texture = load("res://graphics/DisasterScoreBar/DisasterBar" +  load_num + ".png")
