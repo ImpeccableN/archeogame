22ee32f94f1e7637200abff8200228c37d84dde5
ImpeccableN <108433588+ImpeccableN@users.noreply.github.com>
2024-09-13 17:04:51 +0200

    coded ending

--- HeuneburgGame/Minigame3/DiaryLvl3End.gd
+++ HeuneburgGame/Minigame3/DiaryLvl3End.gd
@@ -4,7 +4,7 @@ onready var vid_player = get_node("TgFlip")
 
 
 func _on_Button_toMap3_pressed():
-	get_tree().change_scene("res://Minigame3/ThirdDay_Map.tscn")
+	get_tree().change_scene("res://Minigame3/MessageBeforeOutro.tscn")
 
 
 func _on_nextPage_Button_pressed():
--- HeuneburgGame/Minigame3/ThirdDay_Map.gd
+++ HeuneburgGame/Minigame3/ThirdDay_Map.gd
@@ -3,7 +3,6 @@ extends Node2D
 
 onready var keltenmuseum_image = get_node("Keltenmuseum_Overlay")
 onready var bauwagen_image = get_node("Bauwagen_Overlay")
-onready var video_player = get_node("OutroVideoPlayer")
 export var images_grow_scale := Vector2.ZERO
 
 func _ready():
@@ -12,7 +11,8 @@ func _ready():
 
 
 func _on_Button_Bauwagen_mouse_entered():
-	bauwagen_image.rect_scale = images_grow_scale
+	if not Global.game_finished:
+		bauwagen_image.rect_scale = images_grow_scale
 
 
 func _on_Button_Bauwagen_mouse_exited():
@@ -27,15 +27,8 @@ func _on_Button_Keltenmuseum_mouse_exited():
 	keltenmuseum_image.rect_scale = Vector2.ONE
 
 
-
-func _on_OutroVideoPlayer_finished():
-	get_tree().change_scene("res://Minigame3/DiaryLvl3.tscn")
-
-
 func _on_DisasterScore_disaster_score_finished():
 	if Global.puzzle_done:
-		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-		yield(get_tree().create_timer(2),"timeout")
 		MusicPlayer.stop()
-		video_player.show()
-		video_player.play()
+		Global.game_finished = true
+		get_tree().change_scene("res://Minigame3/DiaryLvl3End.tscn")
